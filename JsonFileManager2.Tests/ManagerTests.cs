f4a2852ebfe7b54574678f71d4def64282006005
HermaN <d.herman85@gmail.com>
2024-01-16 16:08:01 +0100

    add getignore and tests

--- JsonFileManager2.Tests/ManagerTests.cs
+++ JsonFileManager2.Tests/ManagerTests.cs
@@ -42,11 +42,75 @@ namespace JsonFileManager2.Tests
 
         }
 
+        [Fact]
+        public void CreateJsonFromDict_Null_ShouldThrowArgumentNullExcetion()
+        {
+            // Given
+            Dictionary<string, object>? dict = null;
+            Manager manager = new();
+
+            // When
+            Action test = () => manager.CreateJsonFromDict(dict);
+
+            // Then
+            test.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void CreateJsonFromDict_NullValue_ShouldThrowNullReferensExeption()
+        {
+            // Given
+            Dictionary<string, object> dict = new()
+            {
+                {"Name", null },
+                {"Age","3000"},
+                {"Profession","Wizard"}
+            };
+            Manager manager = new();
+
+            // When
+            Action test = () => manager.CreateJsonFromDict(dict);
+
+            // Then
+            test.Should().Throw<NullReferenceException>();
+        }
+
         // Test : Dictionary till JSON
 
         // Test : Läsa JSON-fil
 
         // Test : Skriva JSON-fil
+        [Fact]
+        public void WriteJsonFile_ValidJson_ShouldWriteJsonFile()
+        {
+            // Given
+            object obj = new { Name = "Sam Gamgee", Age = 30, Profession = "Gardener" };
+            string json = JsonConvert.SerializeObject(obj);
+            string fileName = "test.json";
+            string filePath = Path.Combine(Environment.CurrentDirectory, fileName);
+            Manager manager = new();
 
+            // When
+            manager.WriteJsonFile(json, fileName);
+
+            // Then
+            string fileContent = File.ReadAllText(filePath);
+            fileContent.Should().BeEquivalentTo(json);
+        }
+
+        [Fact]
+        public void WriteJsonFile_InvalidJson_ShouldThrowJsonReaderExeption()
+        {
+            // Given
+            string invalidJson = "This is not json";
+            string fileName = "test.json";
+            Manager manager = new();
+
+            // When
+            Action test = () => manager.WriteJsonFile(invalidJson, fileName);
+
+            // Then
+            test.Should().Throw<JsonReaderException>();
+        }
     }
 }
--- JsonFileManager2/Manager.cs
+++ JsonFileManager2/Manager.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace JsonFileManager2
 {
@@ -9,14 +10,37 @@ namespace JsonFileManager2
         // Dictionary till JSON
         public string CreateJsonFromDict(Dictionary<string, object> dict)
         {
-            if (!dict.Any())
-            {
-                throw new ArgumentException();
-            }
+            // Guard clause
+            if (dict == null) { throw new ArgumentNullException(); }
+            else if (!dict.Any()) { throw new ArgumentException(); }
+            else if (dict.ContainsValue(null)) { throw new NullReferenceException(); }
             return JsonConvert.SerializeObject(dict);
         }
+
+
         // Läsa JSON-fil
 
         // Skriva JSON-fil
+        public void WriteJsonFile(string json, string fileName)
+        {
+            // Guard clause
+            if (IsValidJson(json))
+            {
+                string filePath = Path.Combine(Environment.CurrentDirectory, fileName);
+                File.WriteAllText(filePath, json);
+            }
+        }
+
+        /// <summary>
+        /// Validates a string as JSON.
+        /// </summary>
+        /// <param name="stringToValidate"></param>
+        /// <returns>Returns true if the input string is correct JSON, otherwise this method throws a JsonReaderExeption.</returns>
+        /// <exception cref="JsonReaderException"></exception>
+        private bool IsValidJson(string stringToValidate)
+        {
+            JToken.Parse(stringToValidate);
+            return true;
+        }
     }
 }
